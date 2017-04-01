66ba0b3229f920b57689fcca0dbae429af9f6b22
Usman <usmanzaheer1995@gmail.com>
2017-04-01 19:36:03 +0500

    add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,19 @@ const request = require('supertest');
 var { app } = require('./../server');
 var { Todo } = require('./../models/Todo');
 
+const todos = [{
+    text: "First test todo"
+}, {
+    text: "Second test todo"
+}];
+
 beforeEach((done) => {
     //before each test case runs, beforeEach will run
     //in this case, we are removing everything from Todos collection in mongo db before each test
     Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+
+    }).then(() => {
         done();
     });
 });
@@ -28,7 +37,7 @@ describe('POST /Todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,9 +56,21 @@ describe('POST /Todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+         .get('/todos')
+         .expect(200)
+         .expect((response) => {
+             expect(response.body.todos.length).toBe(2)
+         })
+         .end(done);
     });
 });
