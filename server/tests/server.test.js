78d0712fdc4293ae136b05b1a56c75bdf4afd98f
Usman <usmanzaheer1995@gmail.com>
2017-04-01 18:56:52 +0500

    test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect');
+const request = require('supertest');
+
+var { app } = require('./../server');
+var { Todo } = require('./../models/Todo');
+
+beforeEach((done) => {
+    //before each test case runs, beforeEach will run
+    //in this case, we are removing everything from Todos collection in mongo db before each test
+    Todo.remove({}).then(() => {
+        done();
+    });
+});
+
+describe('POST /Todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.text).toBe(text);
+            })
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+
+            });
+    });
+
+    it('should not create todo within invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
